cabfac2167f821d5fbf731819a779e11a3dd5fad
95.nagy.d@gmail.com <Rbableves9>
2018-12-11 17:49:46 +0100

    Server can start with tomcat

---
+++ job-applicant-assignment-web/src/main/java/web/config/WebConfig.java
@@ -0,0 +1,11 @@
+package web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"web.config"})
+public class WebConfig {
+}
---
+++ job-applicant-assignment-web/src/main/java/web/config/WebInitializer.java
@@ -0,0 +1,25 @@
+package web.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("dispatcherServer", new DispatcherServlet(context));
+
+        servletRegistration.setLoadOnStartup(0);
+        servletRegistration.addMapping("/");
+        System.out.println("Server is running");
+    }
+}
